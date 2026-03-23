464fdb6f8367d94e4f6bde2e62cf38f276ea8787
ctrl.x <kuspysbogdan@gmail.com>
2026-03-23 17:51:39 +0100

    feat: add screen-test env with TFT_eSPI for ST7789 240x240 LCD
    - Add screen-test PlatformIO environment with ESP32-S3 board config
    - Configure TFT_eSPI via build_flags (USER_SETUP_LOADED, USE_FSPI_PORT)
    - Pin mapping: MOSI=41, SCLK=40, CS=39, DC=38, RST=42
    - Basic RGB color cycle test for display verification
    - Add .cache to .gitignore

--- firmware/src/screen-test/screen-test.cpp
+++ firmware/src/screen-test/screen-test.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI(240, 240);
+
+#define LCD_BL 20
 
 void setup() {
-  // Setup code here
+  Serial.begin(115200);
+  Serial.println("Screen test starting...");
+
+  // Backlight on
+  pinMode(LCD_BL, OUTPUT);
+  digitalWrite(LCD_BL, HIGH);
+
+  // Init TFT (TFT_eSPI handles SPI.begin internally)
+  tft.begin();
+  tft.invertDisplay(1);
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+
+  Serial.println("TFT initialized.");
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(10, 10);
+  tft.println("Screen Test OK!");
+  delay(1000);
 }
 
 void loop() {
-  // Main screen test loop
+  tft.fillScreen(TFT_RED);
+  tft.setTextColor(TFT_WHITE, TFT_RED);
+  tft.setTextSize(3);
+  tft.setCursor(20, 100);
+  tft.println("RED");
+  delay(2000);
+
+  tft.fillScreen(TFT_GREEN);
+  tft.setTextColor(TFT_BLACK, TFT_GREEN);
+  tft.setCursor(20, 100);
+  tft.println("GREEN");
+  delay(2000);
+
+  tft.fillScreen(TFT_BLUE);
+  tft.setTextColor(TFT_WHITE, TFT_BLUE);
+  tft.setCursor(20, 100);
+  tft.println("BLUE");
+  delay(2000);
 }
